829914a040a1af16d4a476f8edfdb6420ed3319e
meilie389 <bossart.emilie@gmail.com>
2020-09-19 12:29:56 +1100

    add delete tag button

--- src/main/webapp/app/entities/tag/tag-detail.component.ts
+++ src/main/webapp/app/entities/tag/tag-detail.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { ITag } from 'app/shared/model/tag.model';
+import { TagDeleteDialogComponent } from './tag-delete-dialog.component';
 
 @Component({
   selector: 'jhi-tag-detail',
@@ -10,7 +12,7 @@ import { ITag } from 'app/shared/model/tag.model';
 export class TagDetailComponent implements OnInit {
   tag: ITag | null = null;
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  constructor(protected activatedRoute: ActivatedRoute, protected modalService: NgbModal) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ tag }) => (this.tag = tag));
@@ -18,5 +20,10 @@ export class TagDetailComponent implements OnInit {
 
   previousState(): void {
     window.history.back();
+  }
+
+  delete(tag: ITag): void {
+    const modalRef = this.modalService.open(TagDeleteDialogComponent, { size: 'lg', backdrop: 'static' });
+    modalRef.componentInstance.tag = tag;
   }
 }
